e616e85abc221f8c50ddc2c910bd89c0ec49c706
Jan Dohinog <janreydohinog@gmail.com>
2022-03-19 19:46:20 +0800

    edit footer

--- src/container/Footer/Footer.js
+++ src/container/Footer/Footer.js
@@ -6,6 +6,8 @@ import mobile from "../../assets/mobile.png";
 import { AppWrap, MotionWrap } from "../../wrapper";
 // import { client } from "../../client";
 import "./Footer.scss";
+import { BsTwitter, BsInstagram } from "react-icons/bs";
+import { FaFacebookF } from "react-icons/fa";
 
 const Footer = () => {
   const [formData, setFormData] = useState({
@@ -94,6 +96,39 @@ const Footer = () => {
           <h3 className="head-text">Thank you for getting in touch!</h3>
         </div>
       )}
+
+      <div className="footer-info">
+        <div className="peb-footer">PEB Review and Training</div>
+        <div>peb.acads@gmail.com</div>
+        <div>0923-575-1844</div>
+        <div>
+          <a
+            href="https://www.google.com/maps/dir/10.3043304,123.9192443/10.68498,122.98469/@10.5132592,122.8654893,9z/data=!3m1!4b1!4m4!4m3!1m1!4e1!1m0"
+            target="_blank"
+            rel="noreferrer"
+            className="map"
+          >
+            Bacolod City
+          </a>
+        </div>
+        <div>
+          <span>
+            <a
+              href="https://web.facebook.com/PEBacads"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaFacebookF />
+            </a>
+          </span>
+          <span>
+            <BsTwitter />
+          </span>
+          <span>
+            <BsInstagram />
+          </span>
+        </div>
+      </div>
     </>
   );
 };
