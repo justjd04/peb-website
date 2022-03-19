2bad1409d81a5a03e1316d528eff376b5b121834
Jan Dohinog <janreydohinog@gmail.com>
2022-03-19 20:22:21 +0800

    edit links

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,18 +1,35 @@
 import React from "react";
 import { BsTwitter, BsInstagram } from "react-icons/bs";
 import { FaFacebookF } from "react-icons/fa";
+import { SiGooglemaps } from "react-icons/si";
 
 const SocialMedia = () => (
   <div className="app__social">
-    <div>
+    {/* <div>
       <BsTwitter />
-    </div>
+    </div> */}
     <div>
-      <FaFacebookF />
+      <a
+        href="https://web.facebook.com/PEBacads"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FaFacebookF />
+      </a>
     </div>
-    <div>
+    <div className="location-icon-wrap">
+      <a
+        href="https://www.google.com/maps/dir/10.3043304,123.9192443/10.68498,122.98469/@10.5132592,122.8654893,9z/data=!3m1!4b1!4m4!4m3!1m1!4e1!1m0"
+        target="_blank"
+        rel="noreferrer"
+        className="map"
+      >
+        <SiGooglemaps className="location-icon" />
+      </a>
+    </div>
+    {/* <div>
       <BsInstagram />
-    </div>
+    </div> */}
   </div>
 );
 
--- src/container/Footer/Footer.js
+++ src/container/Footer/Footer.js
@@ -8,6 +8,7 @@ import { AppWrap, MotionWrap } from "../../wrapper";
 import "./Footer.scss";
 import { BsTwitter, BsInstagram } from "react-icons/bs";
 import { FaFacebookF } from "react-icons/fa";
+import { SiGooglemaps } from "react-icons/si";
 
 const Footer = () => {
   const [formData, setFormData] = useState({
@@ -121,12 +122,24 @@ const Footer = () => {
               <FaFacebookF />
             </a>
           </span>
+
           <span>
+            <a
+              href="https://www.google.com/maps/dir/10.3043304,123.9192443/10.68498,122.98469/@10.5132592,122.8654893,9z/data=!3m1!4b1!4m4!4m3!1m1!4e1!1m0"
+              target="_blank"
+              rel="noreferrer"
+              className="map"
+            >
+              <SiGooglemaps className="footer-location-icon" />
+            </a>
+          </span>
+
+          {/* <span>
             <BsTwitter />
           </span>
           <span>
             <BsInstagram />
-          </span>
+          </span> */}
         </div>
       </div>
     </>
